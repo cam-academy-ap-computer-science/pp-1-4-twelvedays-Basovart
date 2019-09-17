15f288d598b92bddd2b989d0340945fcd359c63e
basovart000 <basovart000@CAM-LAB-09.bgsd.k12.wa.us>
2019-09-17 07:44:59 -0700

    TwelveDays.

--- src/TwelveDays.java
+++ src/TwelveDays.java
@@ -2,8 +2,68 @@
 public class TwelveDays {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
-
+	public static void FirstDay() {
+		System.out.println("On the first day of Christmas,");
+		TrueLove();
+		Partridge();
+	}
+	
+	public static void SecondDay() {
+		System.out.println("On the second day of Christmas,");
+	}
+	
+	public static void ThirdDay() {
+		System.out.println("On the third day of Christmas,");
+	}
+	
+	public static void FourthDay() {
+		System.out.println("On the fourth day of Christmas,");
+	}
+	
+	public static void FifthDay() {
+		System.out.println("On the fifth day of Christmas,");
+	}
+	
+	public static void SixthDay() {
+		System.out.println("On the sixth day of Christmas,");
+	}
+	
+	public static void SeventhDay() {
+		System.out.println("On the seventh day of Christmas,");
+	}
+	
+	public static void EighthDay() {
+		System.out.println("On the eighth day of Christmas,");
+	}
+	
+	public static void NinthDay() {
+		System.out.println("On the ninth day of Christmas,");
+	}
+	
+	public static void TenthDay() {
+		System.out.println("On the tenth day of Christmas,");
+	}
+	
+	public static void EleventhDay() {
+		System.out.println("On the eleventh day of Christmas,");
+	}
+	
+	public static void TwelfthDay() {
+		System.out.println("On the twelfth day of Christmas,");
+	}
+	
+	public static void TrueLove() {
+		System.out.println("my true love sent to me");
+	}
+	public static void Partridge() {
+		System.out.println("a partridge in a pear tree.\n");
+	}
+	public static void Doves() {
+		System.out.println("two turtle doves, and");
+	}
+	public static void Hen() {
+		System.out.println("three French hens,");
+	}
 }
